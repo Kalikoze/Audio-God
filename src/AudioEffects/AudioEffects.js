73b43236a386afb05507ff2edd3ca17ce0cbf45a
Jason Lucas <jlucas5280@gmail.com>
2017-09-06 17:49:44 -0600

    audio background

--- src/AudioEffects/AudioEffects.js
+++ src/AudioEffects/AudioEffects.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './AudioEffects.css'
+import AudioControlsBack from '../assets/audio-effects-background.png';
 
 export default class AudioEffects extends Component {
   render() {
     return (
       <div className="track-controls">
-        Track Controls
+        <div className='track-controls-inner-box'>
+          <img className='audio-controls-backdrop' src={AudioControlsBack}/>
+        </div>
+
       </div>
     )
   }

9dad95568723c7b919b8b64adbd14a8476f3ea84
Jason Lucas <jlucas5280@gmail.com>
2017-09-08 13:47:55 -0600

    hover states

--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -9,19 +9,19 @@ export default class Controls extends Component {
       <div className="controls">
         <div className='upper-button-container'>
           <div className='login-button-container'>
-            <div className='controls-button-glass'>
+            <div className='controls-button-glass login-glass'>
               <p className='control-button-label'>LOGIN</p>
             </div>
             <img className='gold-button-bezel' alt='' src={goldButton}/>
           </div>
           <div className='create-button-container'>
-            <div className='controls-button-glass'>
+            <div className='controls-button-glass create-glass'>
               <p className='control-button-label'>CREATE ACCT</p>
             </div>
             <img className='gold-button-bezel' alt='' src={goldButton}/>
           </div>
           <div className='logOut-button-container'>
-            <div className='controls-button-glass'>
+            <div className='controls-button-glass logOut-glass'>
               <p className='control-button-label'>LOGOUT</p>
             </div>
             <img className='gold-button-bezel' alt='' src={goldButton}/>
@@ -29,19 +29,19 @@ export default class Controls extends Component {
         </div>
         <div className='lower-button-container'>
           <div className='upload-button-container'>
-            <div className='controls-button-glass'>
+            <div className='controls-button-glass upload-glass'>
               <p className='control-button-label'>UPLOAD</p>
             </div>
             <img className='gold-button-bezel' alt='' src={goldButton}/>
           </div>
           <div className='save-button-container'>
-            <div className='controls-button-glass'>
+            <div className='controls-button-glass save-glass'>
               <p className='control-button-label'>SAVE</p>
             </div>
             <img className='gold-button-bezel' alt='' src={goldButton}/>
           </div>
           <div className='saveAs-button-container'>
-            <div className='controls-button-glass'>
+            <div className='controls-button-glass saveAs-glass'>
               <p className='control-button-label'>SAVE AS</p>
             </div>
             <img className='gold-button-bezel' alt='' src={goldButton}/>
